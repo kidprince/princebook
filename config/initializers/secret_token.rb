8b62e32a4b4629d44fce7943fd1e4927275260e2
Edward Williams <ew.kidprince@gmail.com>
2013-08-20 13:24:33 -0400

    thrid time doing this

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Princebook::Application.config.secret_token = 'acef6e61e32773b3ccb64ecafbd2fe303df3f52fc6746b7f1e6bb505b1a5431835c1a085d6630ffb34402727eca0e055e1023804cb8ba4ad3809db5671f79328'
